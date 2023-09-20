ee199c80fd795135fec24de6c431a9b798381c35
LuisSandoval128 <luis_viper121@hotmail.com>
2023-09-19 22:24:31 -0600

    Rutas de los archivos y seleccion de templates

---
+++ ServicioSocialAD2023/ControlHelpers/ComboBoxHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uanl_ss_main_ui.ControlHelpers
+{
+    internal class ComboBoxHelper
+    {
+        public static string Formato(ComboBox comboBox)
+        {
+            string selectedItem = comboBox.SelectedItem.ToString();
+            switch (selectedItem)
+            {
+                case "Microsoft Word":
+                    return "docx";
+                case "Adobe PDF":
+                    return "pdf";
+                default:
+                    throw new ArgumentException("Formato no válido");
+            }
+        }
+
+        public static string Template(ComboBox comboBox)
+        {
+            string selectedItem = comboBox.SelectedItem.ToString();
+            switch (selectedItem)
+            {
+                case "Boleta de presentación":
+                    return "BPSS";
+                case "Carta de inicio":
+                    return "CISS";
+                case "Kardex":
+                    return "KRD";
+                case "Carta Finalización":
+                    return "CFSS";
+                case "Evaluación":
+                    return "EVL";
+                default:
+                    throw new ArgumentException("Plantilla no válida");
+            }
+        }
+    }
+}
--- ServicioSocialAD2023/ControlHelpers/FileAccessHelper.cs
+++ ServicioSocialAD2023/ControlHelpers/FileAccessHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using uanl_ss_lib_entities_api.GlobalEntities.Dependencies;
 using uanl_ss_lib_xml_local_api;
 using uanl_ss_main_ui.Entities;
 
@@ -26,5 +27,51 @@ namespace uanl_ss_main_ui.ControlHelpers
                 return path;
             }
         }
+
+        
+        public static string DefineExportingRoute(string txtCmb, string Template, CSPrograma programa, string Formato) {
+            
+            string studentDir = $"{programa.Alumno.Matricula}_{programa.Alumno.Nombre}_{programa.Alumno.ApellidoPaterno}_{programa.Alumno.ApellidoMaterno}";
+            //string exportRoute = string.Empty;
+            string exportRoute;
+
+            switch (txtCmb)
+            {
+                case "Estudiante":
+
+                    exportRoute = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), $"UANL Servicio Social\"Archives\"AD2023\"{Template}\"{studentDir}\"{studentDir}.{Formato}");
+
+                    break;
+
+                case "Carrera":
+
+                    string CarreraDir = $"{programa.Alumno.Carrera}";
+                    exportRoute = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), $"UANL Servicio Social\"Archives\"AD2023\"{Template}\"{CarreraDir}\"{studentDir}\"{studentDir}.{Formato}");
+
+                    break;
+
+                case "DependenciaP":
+
+                    string DependenciaPDir = $"{programa.Dependencia}";
+                    exportRoute = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), $"UANL Servicio Social\"Archives\"AD2023\"{Template}\"{DependenciaPDir}\"{studentDir}\"{studentDir}.{Formato}");
+
+                    break;
+
+                case "Responsable":
+
+                    string ResponsableDir = $"{programa.Responsable}";
+                    exportRoute = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), $"UANL Servicio Social\"Archives\"AD2023\"{Template}\"{ResponsableDir}\"{studentDir}\"{studentDir}.{Formato}");
+
+                    break;
+
+
+                default:
+                    throw new ArgumentException("no válido");
+
+            }
+
+            return exportRoute;
+        }
+           
     }
 }
